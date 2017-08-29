e3dbebce0b8be9476d5d9e5fba8983de1b004af9
= <=>
2017-08-29 18:53:56 +0200

    separate classes

---
+++ src/staff.php
@@ -0,0 +1,19 @@
+<?php 
+
+class Staff {
+
+	protected $members = [];
+
+	public function __construct($members = []) {
+		$this->members = $members;
+	}
+	public function add(Person $person) {
+		$this->members[] = $person;
+	}
+	public function members() {
+		return $this->members;
+	}
+	
+}
+
+ ?>
